43f1d6fad2a6b9ee5c8507c0088555f31a42a38c
unicodeveloper <prosperotemuyiwa@gmail.com>
2022-06-02 12:21:56 +0100

    Update storage extend bindings

--- src/CloudinaryServiceProvider.php
+++ src/CloudinaryServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
+use Illuminate\Filesystem\FilesystemAdapter;
 use CloudinaryLabs\CloudinaryLaravel\Commands\BackupFilesCommand;
 use CloudinaryLabs\CloudinaryLaravel\Commands\DeleteFilesCommand;
 use CloudinaryLabs\CloudinaryLaravel\Commands\FetchFilesCommand;
@@ -163,7 +164,14 @@ class CloudinaryServiceProvider extends ServiceProvider
         Storage::extend(
             'cloudinary',
             function ($app, $config) {
-                return new Filesystem(new CloudinaryAdapter(config('cloudinary.cloud_url')));
+
+                $cloudinaryAdapter = new CloudinaryAdapter(config('cloudinary.cloud_url'));
+
+                return new FilesystemAdapter(
+                    new Filesystem($cloudinaryAdapter, $config),
+                    $cloudinaryAdapter,
+                    $config
+                );
             }
         );
     }
