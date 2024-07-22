248e9c99aad8186eacd37adbec32e0ec2cd73e1c
Josh Manders <josh@joshmanders.com>
2024-07-22 15:45:54 -0500

    fix: tests

--- tests/CloudinaryAdapterTest.php
+++ tests/CloudinaryAdapterTest.php
@@ -1,36 +1,29 @@
 <?php
 
 use Orchestra\Testbench\TestCase;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Database\Eloquent\Model;
-use CloudinaryLabs\CloudinaryLaravel\MediaAlly;
-use CloudinaryLabs\CloudinaryLaravel\CloudinaryEngine;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
+use CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider;
 
-/**
- *
- */
 class CloudinaryAdapterTest extends TestCase
 {
 
-    protected function setUp(): void
-    {
-       parent::setUp();
-    }
-
     protected function getEnvironmentSetup($app)
     {
+        $app->useEnvironmentPath(__DIR__.'/..');
+        $app->bootstrapWith([LoadEnvironmentVariables::class]);
+        parent::getEnvironmentSetUp($app);
+
         $app['config']->set('cloudinary.cloud_url', env('CLOUDINARY_URL'));
     }
 
     protected function getPackageProviders($app)
     {
-        return ['CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider'];
+        return CloudinaryServiceProvider::class;
     }
 
     public function test_can_get_url_given_public_id()
     {
         //file already exists
-        $this->assertEquals('https://res.cloudinary.com/dwinzyahj/image/upload/v1616774832/sample_qgqooj.jpg', Storage::disk('cloudinary')->url('sample_qgqooj.jpg'));
+        $this->assertEquals(env('TEST_FILE_URL'), Storage::disk('cloudinary')->url(basename(env('TEST_FILE_URL'))));
     }
 }
