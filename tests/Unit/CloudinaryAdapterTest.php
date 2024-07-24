460f2db0f170afcc00696a0ab50a649044f56eeb
Josh Manders <josh@joshmanders.com>
2024-07-24 16:28:57 -0500

    mock cloudinary

--- tests/Unit/CloudinaryAdapterTest.php
+++ tests/Unit/CloudinaryAdapterTest.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Storage;
+use Mockery\MockInterface;
 
 it('can get url given public id', function () {
-    expect(Storage::disk('cloudinary')->url(basename(env('TEST_FILE_URL'))))->toEqual(env('TEST_FILE_URL'));
+    $file = 'baz.jpg';
+    $url = 'https://res.cloudinary.com/foo/image/upload/bar/' . $file;
+
+    $this->mock('overload:' . Cloudinary\Api\Admin\AdminApi::class, function (MockInterface $mock) use ($url) {
+        $mock->shouldReceive('asset')->once()->andReturn(['secure_url' => $url]);
+    })->makePartial();
+
+    $result = Storage::disk('cloudinary')->url($file);
+    expect($result)->toEqual($url);
 });
--- tests/Unit/MediaAllyTest.php
+++ tests/Unit/MediaAllyTest.php
@@ -1,13 +1,29 @@
 <?php
 
 use Illuminate\Http\UploadedFile;
+use Mockery\MockInterface;
 use Tests\Fixtures\Models\Example;
 
-it('can attach media to a model', function () {
+beforeEach(function () {
+    $this->mock('overload:' . Cloudinary\Api\Upload\UploadApi::class, function (MockInterface $mock) {
+        $mock->shouldReceive('upload')->andReturn([
+            'public_id' => 'file',
+            'bytes' => '123',
+            'secure_url' => 'https://example.com/file.jpg',
+            'resource_type' => 'image',
+        ]);
 
+        $mock->shouldReceive('destroy')->andReturn([
+            'result' => 'ok',
+        ]);
+    })->makePartial();
+});
+
+it('can attach media to a model', function () {
     $example = Example::create([]);
 
     $example->attachMedia(UploadedFile::fake()->image('file.jpg'));
+
 
     expect($example->fetchAllMedia())->toHaveCount(1);
 });
